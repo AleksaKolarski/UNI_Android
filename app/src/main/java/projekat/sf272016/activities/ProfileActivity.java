672fef214b6a758b14450b0f879261ea710ffd90
Aleksa-PC <aleksa.kolarski@yahoo.com>
2018-05-13 19:51:05 +0200

    ProfileActivity has Drawer and Toolbar.

--- app/src/main/java/projekat/sf272016/activities/ProfileActivity.java
+++ app/src/main/java/projekat/sf272016/activities/ProfileActivity.java
@@ -3,18 +3,30 @@ package projekat.sf272016.activities;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 import projekat.sf272016.R;
+import projekat.sf272016.misc.DrawerHelper;
+import projekat.sf272016.misc.IDrawerClickHandler;
+import projekat.sf272016.misc.ToolbarHelper;
+import projekat.sf272016.model.misc.DrawerListItem;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    DrawerHelper drawerHelper;
+    ToolbarHelper toolbarHelper;
+
     private Button changePasswordButton;
     private LinearLayout changePasswordLayout;
     private EditText changePasswordBox1;
@@ -26,10 +38,48 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        /* Drawer */
+        ArrayList<DrawerListItem> drawerListItems = new ArrayList<>();
+        drawerListItems.add(new DrawerListItem("Posts", "See all posts", R.drawable.ic_launcher_foreground));
+        drawerListItems.add(new DrawerListItem("Settings", "Change app settings", R.drawable.ic_launcher_foreground));
+        drawerHelper = new DrawerHelper(this, drawerListItems, new DrawerClickHandler());
+        drawerHelper.initialize();
+
+        /* Toolbar */
+        toolbarHelper = new ToolbarHelper(this);
+        toolbarHelper.initialize();
+
         changePasswordButton = (Button)findViewById(R.id.profileActivityChangePasswordButton);
         changePasswordLayout = (LinearLayout)findViewById(R.id.profileActivityChangePasswordLayout);
         changePasswordBox1 = (EditText)findViewById(R.id.profileActivityChangePasswordBox1);
         changePasswordBox2 = (EditText)findViewById(R.id.profileActivityChangePasswordBox2);
+    }
+
+    private class DrawerClickHandler implements IDrawerClickHandler {
+        @Override
+        public void handleClick(View view, int position){
+            Toast.makeText(getApplicationContext(), ((TextView)((RelativeLayout) view).getChildAt(1)).getText(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_action_new:
+                Toast.makeText(getApplicationContext(), "new" , Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.toolbar_action_sync:
+                Toast.makeText(getApplicationContext(), "sync" , Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void btnProfileChangePassword(View view) {
--- app/src/main/java/projekat/sf272016/activities/ReadPostActivity.java
+++ app/src/main/java/projekat/sf272016/activities/ReadPostActivity.java
@@ -2,7 +2,6 @@ package projekat.sf272016.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,7 +10,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import projekat.sf272016.R;
 import projekat.sf272016.misc.DrawerHelper;
